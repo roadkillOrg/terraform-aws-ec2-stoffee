644deee18f9e340ad93c57ba5ee3eb66653f6a5f
Chris Dunlap <chris@roadkill.org>
2019-02-11 12:47:16 -0800

    fixed to work

--- main.tf
+++ main.tf
@@ -1,7 +1,7 @@
 provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
-  region = "us-east-2"
+  region = "us-west-2"
 }
 
 variable "aws_access_key" {
@@ -30,7 +30,7 @@ data "aws_ami" "ubuntu" {
 
 resource "aws_instance" "web" {
   ami           = "${data.aws_ami.ubuntu.id}"
-  instance_type = "t2.large"
+  instance_type = "t2.micro"
 
   tags = {
     Name = "Demo"
