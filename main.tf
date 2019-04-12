b9ffbeeb89a37ad7de14280afa7f486c18e995d2
Chris Dunlap <chris@roadkill.org>
2019-04-12 09:15:55 -0700

    Changed stuff

--- main.tf
+++ main.tf
@@ -43,8 +43,8 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "demo" {
   ami           = "${data.aws_ami.ubuntu.id}"
   #do not change this from t2.micro, unless you want to trigger sentinel
-  instance_type = "t2.micro"
-#   instance_type = "t2.2xlarge"
+#  instance_type = "t2.micro"
+  instance_type = "t2.2xlarge"
   key_name = "cdunlap-demo"
   
   tags = {
