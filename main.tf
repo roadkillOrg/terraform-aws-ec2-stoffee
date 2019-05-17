eaa330407c5337fb187fdd0161952ea914394587
Chris Dunlap <chris@roadkill.org>
2019-05-17 16:55:39 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -13,7 +13,7 @@ provider "aws" {
   access_key = "${var.aws_access_key}"
   secret_key = "${var.aws_secret_key}"
   #don't change this from us-west-2 :)
-  region = "us-west-2"
+  region = "us-west-1"
 }
 
 variable "aws_access_key" {
@@ -43,14 +43,14 @@ data "aws_ami" "ubuntu" {
 resource "aws_instance" "demo" {
   ami           = "${data.aws_ami.ubuntu.id}"
   #do not change this from t2.micro, unless you want to trigger sentinel
- instance_type = "t2.micro"
- #instance_type = "t2.2xlarge"
+ #instance_type = "t2.micro"
+ instance_type = "t2.2xlarge"
   key_name = "cdunlap-demo"
   
   tags = {
     Name = "cdunlap simple ec2 demo"
     #uncomment this for working, comment out for sentinel policy trigger
- Owner = "cdunlap@hashicorp.com"
+ # Owner = "cdunlap@hashicorp.com"
     TTL = "24h"
       }   
 }
