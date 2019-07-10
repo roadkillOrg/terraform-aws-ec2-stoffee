4552a5a4d6000bff3542ae85df5bbec9b9449912
stoffee <chris@roadkill.org>
2019-07-10 13:10:22 -0700

    0.12upgrade

--- main.tf
+++ main.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "remote" {
-    hostname = "app.terraform.io"
+    hostname     = "app.terraform.io"
     organization = "cdunlap"
 
     workspaces {
@@ -10,8 +10,9 @@ terraform {
 }
 
 provider "aws" {
-  access_key = "${var.aws_access_key}"
-  secret_key = "${var.aws_secret_key}"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+
   #don't change this from us-west-2 :)
   region = "us-west-2"
 }
@@ -41,26 +42,29 @@ data "aws_ami" "ubuntu" {
 }
 
 resource "aws_instance" "demo" {
-  ami           = "${data.aws_ami.ubuntu.id}"
+  ami = data.aws_ami.ubuntu.id
+
   #do not change this from t2.micro, unless you want to trigger sentinel
- instance_type = "t2.micro"
-# instance_type = "t2.2xlarge"
+  instance_type = "t2.micro"
+
+  # instance_type = "t2.2xlarge"
   key_name = "cdunlap-demo"
 
   tags = {
     Name = "cdunlap simple ec2 demo"
     #uncomment this for working, comment out for sentinel policy trigger
-   Owner = "cdunlap@hashicorp.com"
-    TTL = "24hrs"
-      }
+    Owner = "cdunlap@hashicorp.com"
+    TTL   = "24hrs"
+  }
 }
 
 output "private_ip" {
   description = "Private IP of instance"
-  value       = "${join("", aws_instance.demo.*.private_ip)}"
+  value       = join("", aws_instance.demo.*.private_ip)
 }
 
 output "public_ip" {
   description = "Public IP of instance (or EIP)"
-  value       = "${join("", aws_instance.demo.*.public_ip)}"
+  value       = join("", aws_instance.demo.*.public_ip)
 }
+
