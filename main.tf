3b7b2de3fe6c0e514c4838f2250a06b895335369
stoffee <chris@roadkill.org>
2019-07-15 17:45:36 -0700

    pets

--- main.tf
+++ main.tf
@@ -10,10 +10,6 @@ terraform {
 }
 
 resource "random_pet" "server" {
-  keepers = {
-    # Generate a new pet name each time we switch to a new AMI id
-    ami_id = var.ami_id
-  }
 }
 
 provider "aws" {
