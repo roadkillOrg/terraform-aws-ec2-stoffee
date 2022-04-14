2b2a65b49f4f9c8d5a5e94699a558a43169dd05e
stoffee <chris@roadkill.org>
2022-04-14 09:38:59 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -30,7 +30,7 @@ resource "aws_instance" "demo" {
   ami = data.aws_ami.ubuntu.id
   
    #instance_type = "t2.xlarge"
-   instance_type = "t2.micro"
+   instance_type = "t2.small"
 
   key_name = var.ssh_key_name
 
